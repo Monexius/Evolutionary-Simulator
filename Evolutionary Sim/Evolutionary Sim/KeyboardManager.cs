e8ce6a47bca3336a8e2f39236cadfda9d3266c5a
Monexius <37837236+Monexius@users.noreply.github.com>
2019-10-24 17:51:49 +0100

    Adding Fruit
    Bushes added

--- Evolutionary Sim/Evolutionary Sim/KeyboardManager.cs
+++ Evolutionary Sim/Evolutionary Sim/KeyboardManager.cs
@@ -13,14 +13,13 @@ namespace Evolutionary_Sim
     class KeyboardManager
     {
         Map map;
-
         int lastScrollState;
+        
         MouseState lastMouseState = new MouseState();
         KeyboardState lastKeyboardState = new KeyboardState();
 
-        public void HandleInput(GameTime gameTime, Texture2D spriteSheet)
+        public void HandleInput(GameTime gameTime, Texture2D spriteSheet )
         {
-          
             MouseState scrollState = Mouse.GetState();
             MouseState mouseState = Mouse.GetState();
             KeyboardState keyboardState = Keyboard.GetState();
@@ -49,9 +48,14 @@ namespace Evolutionary_Sim
             //Player.Update(gameTime);
             if (mouseState.LeftButton == ButtonState.Pressed && lastMouseState.LeftButton == ButtonState.Released)
             {
+                Map.ClearArray();
                 map = new Map();
-                Map.Initialize(spriteSheet, 2, 4, 32);
+                Map.Initialize(spriteSheet, 2, 4, 33);
+
+               
+                //Map.Draw(spriteBatch);
                 //Tiles,Smoothness of caves, tiles needed, Chance to spawn
+        
                 ScreenTransition.RunAnimation();
             }
             if (keyboardState.IsKeyDown(Keys.W) && lastKeyboardState.IsKeyUp(Keys.W))
